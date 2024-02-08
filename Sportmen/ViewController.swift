1b6abbad5d3fbb5f9def40dd8954043a1073e0c0
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-02-08 09:58:20 +0300

    up

---
+++ Sportmen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sportmen
+//
+//  Created by IREM SEVER on 8.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
