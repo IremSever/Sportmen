4a6bac97d58ca457e4184906557df16ab808d29f
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-02-26 15:58:42 +0300

    up

---
+++ Sportmen/Service/MyTeamWebservice.swift
@@ -0,0 +1,14 @@
+//
+//  MyTeamWebservice.swift
+//  Sportmen
+//
+//  Created by İrem Sever on 26.02.2024.
+//
+
+import Foundation
+
+class MyTeamWebservice {
+    func getMyTeamData (completion: @escaping (Result<MyTeam, Error>) -> Void) {
+        
+    }
+}
