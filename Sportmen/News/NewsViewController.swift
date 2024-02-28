80d7924bb76d80097fb8566b9555574e3075f4d7
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-02-28 07:14:02 +0300

    up

--- Sportmen/News/NewsViewController.swift
+++ Sportmen/News/NewsViewController.swift
@@ -11,12 +11,15 @@ import UIKit
 class NewsViewController: UIViewController {
     @IBOutlet weak var newsTableView: UITableView!
     let newsViewModel = NewsViewModel()
+    var isModifiedList = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.newsTableView.dataSource = self
         self.newsTableView.delegate = self
         loadNewsData()
+        navigationBarDesign()
+        
     }
     private func loadNewsData() {
         newsViewModel.fetchNewsData { [weak self] in
@@ -30,6 +33,39 @@ class NewsViewController: UIViewController {
         if let newsDetailVC = storyboard.instantiateViewController(withIdentifier: "NewsDetailVC") as? NewsDetailVC {
             newsDetailVC.selectedNews = news
             navigationController?.pushViewController(newsDetailVC, animated: true)
+        }
+    }
+    
+    func navigationBarDesign() {
+        let imageView = UIImageView(image: UIImage(named: "icon_mainmenu"))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: imageView)
+        
+        let buttonModifyList = UIButton(type: .custom)
+        buttonModifyList.setImage(UIImage(named: "modifylist"), for: .normal)
+        buttonModifyList.contentMode = .scaleAspectFit
+        buttonModifyList.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        buttonModifyList.addTarget(self, action: #selector(buttonModifyListTapped), for: .touchUpInside)
+        let modifyListBarButton = UIBarButtonItem(customView: buttonModifyList)
+        
+        let buttonMomentNews = UIButton(type: .custom)
+        buttonMomentNews.setImage(UIImage(named: "momentnews"), for: .normal)
+        buttonMomentNews.contentMode = .scaleAspectFit
+        buttonMomentNews.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        buttonMomentNews.addTarget(self, action: #selector(buttonMomentNewsTapped), for: .touchUpInside)
+        let momentNewsBarButton = UIBarButtonItem(customView: buttonMomentNews)
+        
+        navigationItem.rightBarButtonItems = [modifyListBarButton, momentNewsBarButton]
+        
+        newsTableView.reloadData()
+    }
+    
+    @objc func buttonModifyListTapped() {
+        isModifiedList = !isModifiedList
+        newsTableView.reloadData()
+    }
+    @objc func buttonMomentNewsTapped() {
+        if let appStoreURL = URL(string: "https://apps.apple.com/tr/app/moment-haber/id1572296166") {
+            UIApplication.shared.open(appStoreURL, options: [:], completionHandler: nil)
         }
     }
 }
@@ -54,14 +90,20 @@ extension NewsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let news = newsViewModel.cellForRowAt(indexPath: indexPath)
         
-        if indexPath.row % 4 == 0 {
-            let cell = newsTableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! NewsTableViewCell1
-            cell.setCell1WithValuesOf(news)
-            return cell
-        } else {
+        if !isModifiedList {
             let cell = newsTableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! NewsTableViewCell2
             cell.setCell2WithValuesOf(news)
             return cell
+        } else {
+            if indexPath.row % 4 == 0 {
+                let cell = newsTableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! NewsTableViewCell1
+                cell.setCell1WithValuesOf(news)
+                return cell
+            } else {
+                let cell = newsTableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! NewsTableViewCell2
+                cell.setCell2WithValuesOf(news)
+                return cell
+            }
         }
     }
     
